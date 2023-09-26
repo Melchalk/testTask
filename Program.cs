db94d2c6ac7d82035082bd54d5cc7fa0e3f9506d
Марина Мельникова <melm2004@mail.ru>
2023-09-26 13:59:34 +0300

    Сhange

--- Program.cs
+++ Program.cs
@@ -2,38 +2,41 @@
 
 const string mistake = "\nНекорректный ввод\nПовторите попытку\n"; 
 const int poundsToShilling = 20, shillingsToPennies = 12; //Валюты для преобразования
+int poundsToPennies = poundsToShilling * shillingsToPennies;
 
-int CorrectInput(bool isCost)
+const string poundsStr = "Фунты", shillingsStr = "Шиллинги", penniesStr = "Пенсы";
+
+uint CorrectInput(bool isCost)
 {
-    int allPennies = 0; //Суммарное количество пенсов
+    uint allPennies = 0; //Суммарное количество пенсов
     string message = isCost == true ? "сумму" : "количество денег"; //Для различия какое данные сейчас вводятся стоимость или деньги покупателя
     //Считывание данных пока все не будут корректны 
     while (!successParse)
     {
         Console.WriteLine($"----Введите {message}----");
 
-        Console.Write("Фунты: ");
-        if (!int.TryParse(Console.ReadLine(), out int pounds))
+        Console.Write($"{poundsStr}: ");
+        if (!uint.TryParse(Console.ReadLine(), out uint pounds))
         {
             Console.WriteLine(mistake);
             continue;
         }
 
-        Console.Write("Шиллинги: ");
-        if (!int.TryParse(Console.ReadLine(), out int shillings))
+        Console.Write($"{shillingsStr}: ");
+        if (!uint.TryParse(Console.ReadLine(), out uint shillings))
         {
             Console.WriteLine(mistake);
             continue;
         }
 
-        Console.Write("Пенсы: ");
-        if (!int.TryParse(Console.ReadLine(), out int pennies))
+        Console.Write($"{penniesStr}: ");
+        if (!uint.TryParse(Console.ReadLine(), out uint pennies))
         {
             Console.WriteLine(mistake);
             continue;
         }
 
-        allPennies += (pounds * poundsToShilling + shillings) * shillingsToPennies; //Преобразование фунтов и шиллингов в пенсы
+        allPennies += (uint)(pounds * poundsToPennies + shillings * shillingsToPennies); //Преобразование фунтов и шиллингов в пенсы
         allPennies += pennies;
         successParse = true;
     }
@@ -41,9 +44,25 @@ int CorrectInput(bool isCost)
     return allPennies;
 }
 
-int costInPennies = CorrectInput(true); //Преобразование стоимости в пенсы
+uint costInPennies = CorrectInput(true); //Преобразование стоимости в пенсы
 successParse = false; 
-int sumInPennies = CorrectInput(false); //Преобразование денег покупателя
+uint sumInPennies = CorrectInput(false); //Преобразование денег покупателя
 
-
+if (costInPennies == sumInPennies)
+    Console.WriteLine("Нет сдачи");
+else if (costInPennies > sumInPennies)
+    Console.WriteLine("Ошибка");
+else
+{
+    Console.WriteLine("---- Сдача ----");
+    //Преобразование валюты 
+    uint changeInPennies = sumInPennies - costInPennies;
+    uint changePounds = (uint)(changeInPennies / poundsToPennies),
+        changeShilling = (changeInPennies / shillingsToPennies) % changePounds,
+        changePennies = (uint)(changeInPennies % poundsToPennies % shillingsToPennies);
+    //Вывод сдачи
+    if (changePounds > 0) Console.WriteLine($"{poundsStr}: {changePounds}");
+    if (changeShilling > 0) Console.WriteLine($"{shillingsStr}: {changeShilling}");
+    if (changePennies > 0) Console.WriteLine($"{penniesStr}: {changePennies}");
+}
 
