032ff08bbbdf25c337bc12e430836f558b1297f2
Марина Мельникова <melm2004@mail.ru>
2023-09-26 12:31:04 +0300

    TruePounds

--- Program.cs
+++ Program.cs
@@ -1,15 +1,17 @@
 ﻿int pounds, shillings, pennies;
 int sum;
 bool successParse = false;
-const string mistake = "Некоректный ввод\nПовторите попытку";
+const string mistake = "Некоректный ввод\nПовторите попытку\n";
 while (!successParse)
 {
     Console.WriteLine("----Введите сумму----");
 
-    Console.WriteLine("Фунты:");
+    Console.Write("Фунты: ");
     if (!int.TryParse(Console.ReadLine(), out pounds))
     {
         Console.WriteLine(mistake);
         continue;
     }
+
+    successParse = true;
 }
