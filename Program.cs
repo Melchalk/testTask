81ea0d2ca82f17c9265d2e7fff08917a09308c0d
Марина Мельникова <melm2004@mail.ru>
2023-09-26 12:29:38 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿int pounds, shillings, pennies;
+int sum;
+bool successParse = false;
+const string mistake = "Некоректный ввод\nПовторите попытку";
+while (!successParse)
+{
+    Console.WriteLine("----Введите сумму----");
+
+    Console.WriteLine("Фунты:");
+    if (!int.TryParse(Console.ReadLine(), out pounds))
+    {
+        Console.WriteLine(mistake);
+        continue;
+    }
+}
