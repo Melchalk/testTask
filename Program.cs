c05d0bcabc268e92d1ff74057fb849623198faa6
Марина Мельникова <melm2004@mail.ru>
2023-09-26 12:59:06 +0300

    CorrectInput

--- Program.cs
+++ Program.cs
@@ -1,17 +1,49 @@
-﻿int pounds, shillings, pennies;
-int sum;
-bool successParse = false;
-const string mistake = "Некоректный ввод\nПовторите попытку\n";
-while (!successParse)
+﻿bool successParse = false; 
+
+const string mistake = "\nНекоректный ввод\nПовторите попытку\n";
+const int poundsToShilling = 20, shillingsToPennies = 12;
+
+int CorrectInput(bool isSum)
 {
-    Console.WriteLine("----Введите сумму----");
+    int allPennies = 0;
+    string message = isSum == true ? "сумму" : "количество денег";
 
-    Console.Write("Фунты: ");
-    if (!int.TryParse(Console.ReadLine(), out pounds))
+    while (!successParse)
     {
-        Console.WriteLine(mistake);
-        continue;
-    }
+        Console.WriteLine($"----Введите {message}----");
 
-    successParse = true;
+        Console.Write("Фунты: ");
+        if (!int.TryParse(Console.ReadLine(), out int pounds))
+        {
+            Console.WriteLine(mistake);
+            continue;
+        }
+
+        Console.Write("Шиллинги: ");
+        if (!int.TryParse(Console.ReadLine(), out int shillings))
+        {
+            Console.WriteLine(mistake);
+            continue;
+        }
+
+        Console.Write("Пенсы: ");
+        if (!int.TryParse(Console.ReadLine(), out int pennies))
+        {
+            Console.WriteLine(mistake);
+            continue;
+        }
+
+        allPennies += (pounds * poundsToShilling + shillings) * shillingsToPennies;
+        allPennies += pennies;
+        successParse = true;
+    }
+    Console.WriteLine($"Итог: {allPennies} пенсов\n");
+    return allPennies;
 }
+
+int costInPennies = CorrectInput(true);
+successParse = false;
+int sumInPennies = CorrectInput(false); 
+
+
+
